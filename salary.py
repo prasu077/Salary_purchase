a415086b35ddecc20283a9b8461556a6d85ad683
prasu077 <prasannaprabu077@gmail.com>
2025-12-18 20:51:46 +0530

    Purchase prediction Streamlit app

--- salary.py
+++ salary.py
@@ -2,24 +2,21 @@ import streamlit as st
 import numpy as np
 import joblib
 
-# Load model + scaler
-model= joblib.load("knn_bundle.pkl")
-
-st.set_page_config(page_title="Purchase Prediction App", layout="centered")
+# ✅ LOAD MODEL + SCALER
+model, scaler = joblib.load("knn_bundle.pkl")
 
 st.title("🛒 Purchase Prediction App")
-st.write("Enter customer details to predict purchase")
 
-age = st.number_input("Age", min_value=1, max_value=100, value=31)
-salary = st.number_input("Salary", min_value=0, value=35000)
+age = st.number_input("Age", 1, 100, 31)
+salary = st.number_input("Salary", 0, 200000, 35000)
 
 if st.button("Predict"):
     new_data = np.array([[age, salary]])
+    new_data_scaled = scaler.transform(new_data)
 
-
-    prediction = model.predict(new_data)[0]
+    prediction = model.predict(new_data_scaled)[0]
 
     if prediction == 1:
         st.success("✅ Customer WILL Purchase")
     else:
-        st.warning("❌ Customer will NOT Purchase")
+        st.error("❌ Customer will NOT Purchase")
